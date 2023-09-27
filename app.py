c9134325b22fe5b74dd5d17b807c0c9e498132ac
Kevin Knights <kevinknights29@hotmail.com>
2023-09-26 22:47:23 -0500

    added hello world test app

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import flet
+
+
+def main(page: flet.Page):
+    page.add(flet.Text(value="Hello World!"))
+
+
+if __name__ == "__main__":
+    flet.app(
+        target=main, 
+        port=os.getenv("PORT", "7860"),
+        iew=flet.AppView.WEB_BROWSER
+    )
