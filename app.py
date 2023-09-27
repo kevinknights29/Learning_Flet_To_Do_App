e56f23bdd21eee544face026b0f7001725cfa7e6
Kevin Knights <kevinknights29@hotmail.com>
2023-09-26 23:42:21 -0500

    added text box and add task feature

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ import flet
 
 
 def main(page: flet.Page):
-    page.add(flet.Text(value="Hello World!"))
+    def add_clicked(event):
+        page.add(flet.Checkbox(label=new_task.value))
+        new_task.value = ""
+        page.update()
+
+    new_task = flet.TextField(value="What needs to be done?")
+
+    page.add(
+        new_task,
+        flet.FloatingActionButton(
+            icon=flet.icons.ADD,
+            on_click=add_clicked,
+        ),
+    )
 
 
 if __name__ == "__main__":
